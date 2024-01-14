fb4da37b9bb90241e88ec9682a782c35ad6899ca
Xerbelay <148629656+Xerbelay@users.noreply.github.com>
2024-01-14 16:35:30 +0300

    Add files via upload

---
+++ new-year/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<input type="text" v-model="name"><br><br>
+<button @click="change">{{ visible == false ? "получить поздравление" : "спрятать поздравление" }}</button>
+<p v-if="visible">{{ 'Поздравляю с новым годом, ' + name }}</p><br><br>
+<input type="text" v-model="msg">
+<ul>
+  <li v-for="item, index in arr" :key="index">{{ item }}</li>
+</ul>
+<button @click="add">добавить пункт списка</button>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      name: '',
+      visible: false,
+      arr: ['что','как','когда',],
+      msg: '',
+    }
+  },
+  methods:{
+    change(){
+      this.visible = !this.visible
+    },
+    add(){
+      /* this.arr.push(this.msg) певрый номер*/
+      this.arr.unshift(this.msg)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
